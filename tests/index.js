7bbcbd3c7875c250ad551d11330e33f15f21def2
jakobpfeifer135 <138628595+jakobpfeifer135@users.noreply.github.com>
2023-10-16 17:01:41 -0500

    added todo's and dist/ into my git ignore

---
+++ tests/index.js
@@ -0,0 +1,16 @@
+//^ TODO Provide text for a logo- SVG file
+
+//^ TODO prompt for 3 characters and this will be the 3 characters that are on the logo
+
+//^ TODO prompt for text color accept keyword or hexadecimal
+
+//^ TODO prompt for shape selection use the list property and have circle, square, triangle as user options
+
+//^ TODO prompt for shapes color allow for keywords or hexadecimal value
+
+//^ TODO makes a svg file called logo.svg
+
+//^ TODO generate a command inline that says generated logo.svg
+
+//^ TODO when i open the image in browser it is 300x200 pixels
+
