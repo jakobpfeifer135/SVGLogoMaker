5c82cbd34758051f6811441cc370007d38f0bfb1
jakobpfeifer135 <138628595+jakobpfeifer135@users.noreply.github.com>
2023-10-17 17:49:57 -0500

    had to make major changes to file and folder structure to allow index.js to run correctly i noticed issues inside the individual classes afterwards and fixed those i rewrote the questions structure to fall all under one array as well as implemented the write file property, added shape color as well

--- tests/index.js
+++ tests/index.js
@@ -1,6 +1,9 @@
 const inquirer = require('inquirer');
-const shapes = require(`../lib/shapes.js`);
+const Circle = require(`../lib/circle.js`); // Correct the require statements
+const Triangle = require(`../lib/triangle.js`); // Correct the require statements
+const Square = require(`../lib/square.js`); // Correct the require statements
 const { writeFile } = require('fs').promises;
+
 const questions = [
   {
     type: 'input',
@@ -32,25 +35,19 @@ const questions = [
     message: 'Select a shape for your logo:',
     choices: ['circle', 'square', 'triangle'],
   },
-    {
-        type: 'input',
-        name: 'shapeColor',
-        message: 'Enter the color for the shape background  (color keyword or hexadecimal):',
-        validate: function (input) {
-          if (isValidColorKeyword(input) || isValidHexColor(input)) {
-            return true;
-          } else {
-            return 'Please enter a valid color keyword or hexadecimal value.';
-          }
-        },
-    }
-]
-
-
-
-
-
-
+  {
+    type: 'input',
+    name: 'shapeColor',
+    message: 'Enter the color for the shape background (color keyword or hexadecimal):',
+    validate: function (input) {
+      if (isValidColorKeyword(input) || isValidHexColor(input)) {
+        return true;
+      } else {
+        return 'Please enter a valid color keyword or hexadecimal value.';
+      }
+    },
+  }
+];
 
 function isValidColorKeyword(input) {
   const validColorKeywords = ['red', 'green', 'blue', 'black', 'white', 'gray'];
@@ -61,23 +58,30 @@ function isValidHexColor(input) {
   return /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/.test(input);
 }
 
-
-
-
-
-
-
-
-
 inquirer.prompt(questions)
-.then((answers) => {
-const shape = new shapes(answers.logoText, answers.textColor, answers.selectedShape, answers.shapeColor)
-return render()
-
-})
-.then((svgLogo) => {
-    return
-})
+  .then((answers) => {
+    if (answers.selectedShape === 'circle') {
+      const circles = new Circle(answers.logoText, answers.textColor, answers.shapeColor); // Use 'Circle' instead of 'circle'
+      return circles.render();
+    }
+    if (answers.selectedShape === 'triangle') {
+      const triangles = new Triangle(answers.logoText, answers.textColor, answers.shapeColor); // Use 'Triangle' instead of 'triangle'
+      return triangles.render();
+    }
+    if (answers.selectedShape === 'square') {
+      const squares = new Square(answers.logoText, answers.textColor, answers.shapeColor); // Use 'Square' instead of 'square'
+      return squares.render();
+    }
+  })
+  .then((svgLogo) => {
+    return writeFile("../examples/logo.svg", svgLogo);
+  })
+  .then(() => {
+    console.log("generated logo.svg!");
+  })
+  .catch((error) => {
+    console.error('An error occurred:', error);
+  });
 
 
 
@@ -106,9 +110,7 @@ return render()
 //     console.log(`You selected the shape color: ${shapeColor}`);
     
 //   })
-  .catch(error => {
-    console.error('An error occurred:', error);
-  });
+  
 
 
 
