6649156db0a7fa2bd0a7bfca4690e0991946772e
jakobpfeifer135 <138628595+jakobpfeifer135@users.noreply.github.com>
2023-10-17 18:32:24 -0500

    added comments to my code

--- tests/index.js
+++ tests/index.js
@@ -1,9 +1,11 @@
 const inquirer = require('inquirer');
-const Circle = require(`../lib/circle.js`); // Correct the require statements
-const Triangle = require(`../lib/triangle.js`); // Correct the require statements
-const Square = require(`../lib/square.js`); // Correct the require statements
-const { writeFile } = require('fs').promises;
+const Circle = require(`../lib/circle.js`); // pulls the circle coding
+const Triangle = require(`../lib/triangle.js`); // pulls the triangles coding
+const Square = require(`../lib/square.js`);  // pulls the square coding
+const { writeFile } = require('fs').promises; // use this out of the fs provided code to write it to the page
 
+
+// the array of questions with their input type and the question we want displayed to the user as well as edge cases to catch user errors
 const questions = [
   {
     type: 'input',
@@ -48,28 +50,28 @@ const questions = [
     },
   }
 ];
-
+// an array of colors that can be used by the user
 function isValidColorKeyword(input) {
   const validColorKeywords = ['red', 'green', 'blue', 'black', 'white', 'gray'];
   return validColorKeywords.includes(input.toLowerCase());
 }
-
+// is used for pulling hexadecimal values and checking to see if the users response matches a value provided
 function isValidHexColor(input) {
   return /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/.test(input);
 }
-
+// checks to see which shape is selected and then applying the matching colors and text to that shape.
 inquirer.prompt(questions)
   .then((answers) => {
     if (answers.selectedShape === 'circle') {
-      const circles = new Circle(answers.logoText, answers.textColor, answers.shapeColor); // Use 'Circle' instead of 'circle'
+      const circles = new Circle(answers.logoText, answers.textColor, answers.shapeColor); 
       return circles.render();
     }
     if (answers.selectedShape === 'triangle') {
-      const triangles = new Triangle(answers.logoText, answers.textColor, answers.shapeColor); // Use 'Triangle' instead of 'triangle'
+      const triangles = new Triangle(answers.logoText, answers.textColor, answers.shapeColor); 
       return triangles.render();
     }
     if (answers.selectedShape === 'square') {
-      const squares = new Square(answers.logoText, answers.textColor, answers.shapeColor); // Use 'Square' instead of 'square'
+      const squares = new Square(answers.logoText, answers.textColor, answers.shapeColor); 
       return squares.render();
     }
   })
@@ -77,7 +79,7 @@ inquirer.prompt(questions)
     return writeFile("../examples/logo.svg", svgLogo);
   })
   .then(() => {
-    console.log("generated logo.svg!");
+    console.log("generated logo.svg!");// on success paste this in the command prompt
   })
   .catch((error) => {
     console.error('An error occurred:', error);
@@ -88,46 +90,4 @@ inquirer.prompt(questions)
 
 
 
-//   .then(answers => {
-//     const logoText = answers.logoText;
-//     console.log(`You entered: ${logoText}`);
-
-    
-//   })
-//   .then(answers => {
-//     const logoColor = answers.logoColor;
-//     console.log(`You selected the color: ${logoColor}`);
-
-    
-//   })
-//   .then(answers => {
-//     const selectedShape = answers.selectedShape;
-//     console.log(`You selected the shape: ${selectedShape}`);
-    
-//   })
-//   .then(answers => {
-//     const shapeColor = answers.shapeColor;
-//     console.log(`You selected the shape color: ${shapeColor}`);
-    
-//   })
-  
-
-
-
-
-
-
-
-
-
-
-//^ TODO prompt for shape selection use the list property and have circle, square, triangle as user options
-
-//^ TODO prompt for shapes color allow for keywords or hexadecimal value
-
-//^ TODO makes a svg file called logo.svg
-
-//^ TODO generate a command inline that says generated logo.svg
-
-//^ TODO when i open the image in browser it is 300x200 pixels
 
